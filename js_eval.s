9784886fb157b98e1571b60626016fc3b4475126
oscar <oscar@a-10-27-10-34.dynapool.vpn.nyu.edu>
2021-04-08 12:59:46 +0800

    eval update

---
+++ js_eval.s
@@ -0,0 +1,21 @@
+#!/bin/sh -e
+#SBATCH --time=24:00:00
+#SBATCH --job-name='testing'
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=50GB
+#SBATCH --gres=gpu:1
+
+module purge
+module load anaconda3/2020.07
+module load cuda/11.1.74
+module load gcc/10.2.0
+
+
+# Replace with your NetID
+NETID=mw3706
+source activate python36
+cd /scratch/${NETID}/dim/Deep_Image_Matting_Reproduce
+
+python eval.py
