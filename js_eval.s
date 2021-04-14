b13f5ccc796b8754c99d80d71148d1bf9727aabf
oscar <oscar@b-10-27-66-88.dynapool.vpn.nyu.edu>
2021-04-14 22:46:30 +0800

    singularity update

--- js_eval.s
+++ js_eval.s
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 #SBATCH --time=24:00:00
-#SBATCH --job-name='testing'
+#SBATCH --job-name='evaluating'
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
@@ -8,14 +8,17 @@
 #SBATCH --gres=gpu:1
 
 module purge
-module load anaconda3/2020.07
-module load cuda/11.1.74
-module load gcc/10.2.0
-
 
 # Replace with your NetID
 NETID=mw3706
-source activate python36
-cd /scratch/${NETID}/dim/Deep_Image_Matting_Reproduce
+cd /scratch/${NETID}/dim
 
+singularity exec --nv \
+	    --overlay /scratch/mw3706/dim/overlay-5GB-200K.ext3:ro \
+	    /scratch/work/public/singularity/cuda11.1-cudnn8-devel-ubuntu18.04.sif \
+	    /bin/bash -c "
+source /ext3/env.sh
+cd Deep_Image_Matting_Reproduce
 python eval.py
+exit
+"
