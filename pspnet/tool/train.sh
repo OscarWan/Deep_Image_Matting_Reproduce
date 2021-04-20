0e7fc38372cbb6c332707a72528b8e22a3d13e24
oscar <oscar@10-209-67-37.dynapool.shanghai.nyu.edu>
2021-04-20 22:38:55 +0800

    psp update

--- pspnet/tool/train.sh
+++ pspnet/tool/train.sh
@@ -19,28 +19,27 @@ singularity exec --nv --overlay /home/mw3706/pytorch-1.1.0.ext3:ro \
         /scratch/work/public/singularity/cuda9.0-cudnn7-devel-ubuntu16.04-20201127.sif \
         /bin/bash -c "
 cd Deep_Image_Matting_Reproduce/pspnet
-export PYTHONPATH=./
-eval "$(conda shell.bash hook)"
 source /ext3/env.sh  # pytorch 1.4.0 env
 PYTHON=python
 
-dataset=$1
-exp_name=$2
-exp_dir=exp/${dataset}/${exp_name}
+exp_dir=exp/portrait/psp101
 model_dir=${exp_dir}/model
 result_dir=${exp_dir}/result
-config=config/${dataset}/${dataset}_${exp_name}.yaml
+config=config/portrait/portrait_psp101.yaml
 now=$(date +"%Y%m%d_%H%M%S")
 
-mkdir -p ${model_dir} ${result_dir}
-cp tool/train.sh tool/train.py tool/test.sh tool/test.py ${config} ${exp_dir}
+cp tool/train.sh ${exp_dir}
+cp tool/train.py ${exp_dir}
+cp tool/test.sh ${exp_dir}
+cp tool/test.py ${exp_dir}
+cp ${config} ${exp_dir}
 
 export PYTHONPATH=./
 $PYTHON -u ${exp_dir}/train.py \
   --config=${config} \
-  2>&1 | tee ${model_dir}/train-$now.log
+  2>&1 | tee ${model_dir}/train-$now.txt
 
 $PYTHON -u ${exp_dir}/test.py \
   --config=${config} \
-  2>&1 | tee ${result_dir}/test-$now.log
+  2>&1 | tee ${result_dir}/test-$now.txt
 "
