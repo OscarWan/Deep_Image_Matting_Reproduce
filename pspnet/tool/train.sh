894e80f6e3790c9559faa5b3d816fc614bd40905
oscar <oscar@10-209-67-37.dynapool.shanghai.nyu.edu>
2021-04-21 15:11:53 +0800

    psp update

--- pspnet/tool/train.sh
+++ pspnet/tool/train.sh
@@ -4,9 +4,9 @@
 #SBATCH --gres=gpu:4
 #SBATCH --time=24:00:00
 #SBATCH --job-name='pspnet'
-##SBATCH --nodes=1
-##SBATCH --ntasks-per-node=1
-##SBATCH --cpus-per-task=16
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=100GB
 
 module purge
