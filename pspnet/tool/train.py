5dbbf3d782cff714582b8a2727a2c45e4cc92904
oscar <oscar@10-209-67-37.dynapool.shanghai.nyu.edu>
2021-04-25 21:10:14 +0800

    train update

--- pspnet/tool/train.py
+++ pspnet/tool/train.py
@@ -200,7 +200,8 @@ def main_worker(gpu, ngpus_per_node, argss):
         transform.ToTensor(),
         transform.Normalize(mean=mean, std=std)])
     train_data = dataset.SemData(split='train', data_root=args.data_root, data_list=args.train_list, transform=train_transform)
-    print("train data is:", train_data.getitem(train_data.data_list[0])[1])
+    image, label = __getitem__(train_data, 0)
+    print("train data label is:", label)
     if args.distributed:
         train_sampler = torch.utils.data.distributed.DistributedSampler(train_data)
     else:
