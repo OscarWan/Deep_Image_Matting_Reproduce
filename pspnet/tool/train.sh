e8f348e94e4ec4e4e3b57a44859d248601b221af
oscar <oscar@10-209-67-37.dynapool.shanghai.nyu.edu>
2021-04-26 22:38:55 +0800

    train update

--- pspnet/tool/train.sh
+++ pspnet/tool/train.sh
@@ -16,7 +16,7 @@ NETID=mw3706
 cd /scratch/${NETID}/dim/Deep_Image_Matting_Reproduce/pspnet
 
 cp tool/train.sh tool/train.py tool/test.sh tool/test.py \
-        config/voc2012/portrait_psp101.yaml exp/portrait/pspnet101
+        config/voc2012/voc2012_psp101.yaml exp/voc2012/pspnet101
 
 singularity exec --nv --overlay /home/mw3706/pytorch-1.1.0.ext3:ro \
         /scratch/work/public/singularity/cuda9.0-cudnn7-devel-ubuntu16.04-20201127.sif \
@@ -24,12 +24,12 @@ singularity exec --nv --overlay /home/mw3706/pytorch-1.1.0.ext3:ro \
 source /ext3/env.sh  # pytorch 1.4.0 env
 
 export PYTHONPATH=./
-python -u exp/portrait/pspnet101/train.py \
-  --config=config/portrait/portrait_psp101.yaml \
-  2>&1 | tee exp/portrait/pspnet101/model/train_log.txt
+python -u exp/voc2012/pspnet101/train.py \
+  --config=config/voc2012/portrait_psp101.yaml \
+  2>&1 | tee exp/voc2012/pspnet101/model/train_log.txt
 
 
-python -u exp/portrait/pspnet101/test.py \
-  --config=config/portrait/portrait_psp101.yaml \
-  2>&1 | tee exp/portrait/pspnet101/result/test_log.txt
+python -u exp/voc2012/pspnet101/test.py \
+  --config=config/voc2012/portrait_psp101.yaml \
+  2>&1 | tee exp/voc2012/pspnet101/result/test_log.txt
 "
