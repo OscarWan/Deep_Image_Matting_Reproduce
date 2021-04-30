f67af36d19dc27432617495541ad836a634f0f70
oscar <oscar@10-209-67-37.dynapool.shanghai.nyu.edu>
2021-04-30 20:14:03 +0800

    portrait update

--- pspnet/util/transform.py
+++ pspnet/util/transform.py
@@ -241,7 +241,7 @@ class BGR2RGB(object):
 class LabelRescale(object):
     # Converts label from grey image to matting label (bg=1, fg=2, al=0)
     def __call__(self, image, label):
-        range_label = np.max(label)-np.min(label)
+        range_label = np.amax(label) - np.amin(label)
         label = label / range_label
         shape = label.shape
         new_label = np.zeros(shape)
