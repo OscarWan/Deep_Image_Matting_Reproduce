ce929bb7f7e900c2116cd3d53e11a98c7a5e62d8
oscar <oscar@10-209-67-37.dynapool.shanghai.nyu.edu>
2021-05-02 11:42:12 +0800

    portrait update

--- pspnet/tool/train.sh
+++ pspnet/tool/train.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # uncomment for slurm
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 #SBATCH --time=24:00:00
 #SBATCH --job-name='pspnet'
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=100GB
 
 module purge
